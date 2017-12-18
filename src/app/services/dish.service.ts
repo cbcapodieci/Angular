42b71ed771a0fa19e2371d7566f0f2a787c30917
Bryce Capodieci <brycecapodieci@gmail.com>
2017-12-17 21:56:46 -0500

    HTTP Part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -11,6 +11,8 @@ import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
+import { error } from 'util';
+
 
 @Injectable()
 export class DishService {
@@ -18,24 +20,28 @@ export class DishService {
   constructor(private http: Http,
     private processHTTPMsgService: ProcessHTTPMsgService) { }
 
-  getDishes(): Observable<Dish[]> {
-    return this.http.get(baseURL + 'dishes')
-      .map(res => { return this.processHTTPMsgService.extractData(res); });
-  }
-
-  getDish(id: number): Observable<Dish> {
-    return  this.http.get(baseURL + 'dishes/'+ id)
-      .map(res => { return this.processHTTPMsgService.extractData(res); });
-  }
-
-  getFeaturedDish(): Observable<Dish> {
-    return this.http.get(baseURL + 'dishes?featured=true')
-      .map(res => { return this.processHTTPMsgService.extractData(res)[0]; });
-  }
-
-  getDishIds(): Observable<number[]> {
-    return this.getDishes()
-      .map(dishes => { return dishes.map(dish => dish.id) });
+    getDishes(): Observable<Dish[]> {
+      return this.http.get(baseURL + 'dishes')
+        .map(res => { return this.processHTTPMsgService.extractData(res); })
+        .catch(error => { return this.processHTTPMsgService.handleError(error); });
+    }
+  
+    getDish(id: number): Observable<Dish> {
+      return  this.http.get(baseURL + 'dishes/'+ id)
+        .map(res => { return this.processHTTPMsgService.extractData(res); })
+        .catch(error => { return this.processHTTPMsgService.handleError(error); });
+    }
+  
+    getFeaturedDish(): Observable<Dish> {
+      return this.http.get(baseURL + 'dishes?featured=true')
+        .map(res => { return this.processHTTPMsgService.extractData(res)[0]; })
+        .catch(error => { return this.processHTTPMsgService.handleError(error); });
+    }
+  
+    getDishIds(): Observable<number[]> {
+      return this.getDishes()
+      .map(dishes => { return dishes.map(dish => dish.id ); })
+      .catch(error => { return Observable.of(error); });
   }
 
 }

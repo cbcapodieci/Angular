c9c004834522fc9f2c1beef25c6d8e3c0a3d23ee
Bryce Capodieci <brycecapodieci@gmail.com>
2017-12-19 20:15:05 -0500

    Fixing build error for username and password for login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { MdDialog, MdDialogRef } from '@angular/material';
+import { MdDialog, MdDialogRef } from "@angular/material";
+
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,21 +10,14 @@ import { MdDialog, MdDialogRef } from '@angular/material';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  
-  user = {remember: false};
-  
-    constructor(public dialogRef: MdDialogRef<LoginComponent>) { } //This how you define the variable dialogRef in the 
-                                                                    // constructor. This will return the reference 
-                                                                    // to my dialog which contains the login component.
 
-    ngOnInit() {
-    }
-  
-    onSubmit() {
-      console.log("User: ", this.user); // console.log("User: ", this.user); This is used to replace the server-side
-                                        // authentication. The console.log allows us to capture the information and
-                                        // view it for development purposes in the Developers Tool in the browser.
-      this.dialogRef.close();
-    }
-  
+  user = {username:'', password: '', remember:false};
+  constructor(private dialogref: MdDialogRef<LoginComponent>) { }
+
+  ngOnInit() {
   }
+  onSubmit() {
+    console.log("user: ", this.user);
+    this.dialogref.close();
+  }
+}
